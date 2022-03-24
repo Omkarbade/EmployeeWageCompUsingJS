31eb7b124bd5a4a7e8ba7adcbef07a5c6bb0a884
Omkarbade <omakrbade3799@gmail.com>
2022-03-24 15:50:50 +0530

    [Omkar] Add Calculate wage total month Program

--- EmployeeComp.js
+++ EmployeeComp.js
@@ -1,4 +1,11 @@
-const IS_ABSENT =0;                ///UC1 ABSENT OR PRESENT
+const IS_ABSENT =0;                      //UC1- absent or present
+const IS_PART_TIME =1;
+const IS_FULL_TIME =2;
+const PART_TIME_HRS =4;
+const FULL_TIME_HRS =8;
+const WAGE_PER_HRS =20;
+const WORKING_DAYS_IN_MONTH = 20;
+const MAX_WORKING_HRS_IN_MONTH = 100;
 
 let checkStatus  = Math.floor(Math.random() * 10) % 2;
 
@@ -11,14 +18,9 @@ else
     console.log("Employee is Present!!");
 }
 
-const IS_PART_TIME =1;                 ///UC2  CAL DAILY WAGE
-const IS_FULL_TIME =2;
-const PART_TIME_HRS =4;
-const FULL_TIME_HRS =8;
-const WAGE_PER_HRS =20;
-let empHrs = 0;
+let empHrs = 0;                         ///UC2
 
-function getWorkingHrs(checkStatus)             ///UC3 REFACTOR
+function getWorkingHrs(checkStatus)               ///UC3 Refactor write function
 {
     switch(checkStatus)
     {
@@ -36,15 +38,25 @@ function getWorkingHrs(checkStatus)             ///UC3 REFACTOR
 checkStatus = Math.floor(Math.random() * 10) % 3;
 empHrs = getWorkingHrs(checkStatus)
 let empWage = empHrs * WAGE_PER_HRS;
-console.log("Employee wage is : "+empWage);
+console.log("Employee wage for a day is : "+empWage);
 
-
-const WORKING_DAYS_IN_MONTH = 20;       //uc4 working days in month
-
-for(let i = 0; i < WORKING_DAYS_IN_MONTH ; i++)
+for(let i = 0; i < WORKING_DAYS_IN_MONTH ; i++)           ///UC4 Calculate Working days in month
 {
     empHrs += getWorkingHrs(checkStatus) ;
 }
 
 empWage = empHrs * WAGE_PER_HRS;
-console.log("Total Work hrs :"+empHrs+" Total Employee wage for a Month is : "+empWage);
+console.log("Total Work hrs : "+empHrs+"  Total Employee wage for a Month is : "+empWage);
+
+
+
+let total_empHrs = 0;                               ///UC5 Cal total working days
+let total_workingDays = 0;         
+
+while(total_empHrs <= MAX_WORKING_HRS_IN_MONTH && total_workingDays < WORKING_DAYS_IN_MONTH)
+{
+    total_workingDays++;
+    total_empHrs += getWorkingHrs(checkStatus);
+}
+empWage = total_empHrs * WAGE_PER_HRS;
+console.log("Total Working days: "+total_workingDays+"  Total working HRS :"+total_empHrs+"  Total Employee wage : "+empWage);
