a91047663c8a6b752f857f5a24cc6400b3ef8c8e
Omkarbade <omakrbade3799@gmail.com>
2022-03-24 15:32:05 +0530

    [Omkar] Add Refactor   Program

--- EmployeeComp.js
+++ EmployeeComp.js
@@ -1,4 +1,4 @@
-const IS_ABSENT =0;
+const IS_ABSENT =0;                ///UC1 ABSENT OR PRESENT
 
 let checkStatus  = Math.floor(Math.random() * 10) % 2;
 
@@ -11,25 +11,29 @@ else
     console.log("Employee is Present!!");
 }
 
-const IS_PART_TIME =1;
+const IS_PART_TIME =1;                 ///UC2  CAL DAILY WAGE
 const IS_FULL_TIME =2;
 const PART_TIME_HRS =4;
 const FULL_TIME_HRS =8;
 const WAGE_PER_HRS =20;
 let empHrs = 0;
 
-checkStatus = Math.floor(Math.random() * 10) % 3;
-switch(checkStatus)
+function getWorkingHrs(checkStatus)             ///UC3 REFACTOR
 {
-    case IS_PART_TIME:
-        empHrs = PART_TIME_HRS;
+    switch(checkStatus)
+    {
+        case IS_PART_TIME:
+            return PART_TIME_HRS;
         break;
-    case IS_FULL_TIME:
-        empHrs = FULL_TIME_HRS;
-        break;
-    default:
-        empHrs = 0;
+        case IS_FULL_TIME:
+            return FULL_TIME_HRS;
+            break;
+        default:
+            return 0;
+    }
 }
 
+checkStatus = Math.floor(Math.random() * 10) % 3;
+empHrs = getWorkingHrs(checkStatus)
 let empWage = empHrs * WAGE_PER_HRS;
 console.log("Employee wage is : "+empWage);
